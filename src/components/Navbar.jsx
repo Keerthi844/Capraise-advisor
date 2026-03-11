b0f1a29fb4267addefc58517a44f574643650c6a
Sameer <sameershaikzayn@gmail.com>
2026-03-11 15:47:21 +0530

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
   return (
     <nav className="bg-white shadow-lg sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+        <div className="flex justify-between items-center h-20">
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-3">
             <img src={Images.logo} alt="Capraise Advisors" className="h-10 w-10" />
